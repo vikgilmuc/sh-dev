4a1035955ebee874bede93348857554e0432eab8
vikgilmuc <victor@victorgil.name>
2014-06-10 14:37:55 +0200

    before new branch

---
+++ administrator/smiley-homes/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-06-07 22:30:56 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-06-07T22:30:56+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-06-07T22:31:02+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
