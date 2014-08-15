ca564b121aa0a7cd903107ecb1a108065eae9a71
vikgilmuc <victor@victorgil.name>
2014-08-15 15:10:29 +0200

    permissions changed

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-if (version_compare(PHP_VERSION, '5.3.10', '<'))
+if (version_compare(PHP_VERSION, '5.3.1', '<'))
 {
 	die('Your host needs to use PHP 5.3.10 or higher to run this version of Joomla!');
 }
